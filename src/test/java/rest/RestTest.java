62ae28c00daafcb3cb4b11a62638cec97fa820b3
Max <photoshot.98@mail.ru>
2022-06-12 19:41:15 +0300

    Create, Update, Delete Methods added (or modified)

--- src/test/java/rest/RestTest.java
+++ src/test/java/rest/RestTest.java
@@ -1,14 +1,77 @@
 package rest;
 
-import io.restassured.RestAssured;
-import org.testng.annotations.Test;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
+import pojos.CreateUserRequest;
+import pojos.CreateUserResponse;
+import pojos.UpdateUserResponce;
+import pojos.Users;
+import steps.UserSteps;
+import utils.RestWrapper;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static steps.UserSteps.*;
+
+
 
 public class RestTest {
 
-    @Test
-    public static void test1 (){
-        RestAssured.given().baseUri("https://reqres.in/api/users?page=1").when().get().then().statusCode(200);
+    private static RestWrapper api;
 
+    @BeforeAll
+    public static void prepareClient (){
+        api = RestWrapper.
+                loginAs("eve.holt@reqres.in", "cityslicks");
     }
 
+    @Test
+    public void getUsers(){
+        List<Users> user = api.getUsers();
+        assertThat(user)
+                .extracting(Users::getEmail)
+                .contains("janet.weaver@reqres.in");
+    }
+    @Test
+    public void createUser() {
+        CreateUserResponse user = RestTest.api.createUser();
+        assertThat(user).extracting(CreateUserResponse::getName).isEqualTo(user.getName());
+        Assertions.assertThat(user).extracting(CreateUserResponse::getJob).isEqualTo(user.getJob());
+    }
+
+
+    @Test
+    public  void checkUsersParameters() {
+
+        Assertions.assertThat(api.getUsers()).extracting(Users::getId).containsExactly(1, 2, 3, 4, 5, 6);
+        Assertions.assertThat(api.getUsers()).extracting(Users::getfirstName).containsExactly("George", "Janet", "Emma", "Eve", "Charles", "Tracey");
+        Assertions.assertThat(api.getUsers()).extracting(Users::getlastName).containsExactly("Bluth", "Weaver", "Wong", "Holt", "Morris", "Ramos");
+    }
+
+    @Test
+    public  void checkSingleUserParameters() {
+        assertThat(api.getSingleUser()).extracting(Users::getId).isEqualTo(2);
+        Assertions.assertThat(api.getSingleUser()).extracting(Users::getfirstName).isEqualTo("Janet");
+        Assertions.assertThat(api.getSingleUser()).extracting(Users::getlastName).isEqualTo("Weaver");
+    }
+    @Test
+    public void checkUpdateUserParamaters(){
+        assertThat(api.updateUser()).extracting(UpdateUserResponce::getName).isEqualTo("Nick");
+        assertThat(api.updateUser()).extracting(UpdateUserResponce::getJob).isEqualTo("QA-engineer");
+    }
+    @Test
+    public void checkPatchUserParameters(){
+        assertThat(api.patchUser()).extracting(UpdateUserResponce::getName).isEqualTo("Nick");
+        assertThat(api.patchUser()).extracting(UpdateUserResponce::getJob).isEqualTo("QA-engineer");
+    }
+    @Test
+    public void deleteUser (){
+        api.deleteUser();
+    }
+
+
 }
+
---
+++ src/test/java/utils/RestWrapper.java
@@ -0,0 +1,99 @@
+package utils;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.http.Cookie;
+import io.restassured.http.Cookies;
+import io.restassured.specification.RequestSpecification;
+import pojos.*;
+import steps.UserSteps;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static utils.UserGenerator.createSimpleUser;
+
+public class RestWrapper {
+    private static final String BASE_URL = "https://reqres.in/api/";
+    private static RequestSpecification REQ_SPEC;
+    private Cookies cookie;
+
+    private RestWrapper(Cookies cookie) {
+        this.cookie = cookie;
+        REQ_SPEC = new RequestSpecBuilder().setBaseUri("https://reqres.in/api/")
+                .setContentType(ContentType.JSON)
+
+                .addCookies(cookie)
+                .build();
+    }
+
+    public static RestWrapper loginAs(String login, String password) {
+        Cookies cookies = given().baseUri("https://reqres.in/api/").basePath("login/").contentType(ContentType.JSON)
+                .body(new UserLogin(login, password))
+                .when().post()
+                .getDetailedCookies();
+
+        return new RestWrapper(cookies);
+    }
+
+    public CreateUserResponse createUser() {
+        CreateUserRequest rq = createSimpleUser();
+
+        CreateUserResponse rs = RestAssured.given().spec(REQ_SPEC)
+                .basePath("users/")
+                .body(rq)
+                .when()
+                .post()
+                .then()
+                .log()
+                .all()
+                .statusCode(201)
+                .extract()
+                .body()
+                .as(CreateUserResponse.class);
+        return rs;
+    }
+
+    public List<Users> getUsers() {
+        return RestAssured.given().log().body().spec(REQ_SPEC)
+                .basePath("users")
+                .when()
+                .get()
+
+                .then()
+                //.log().body()
+                .statusCode(200)
+                .extract()
+                .jsonPath()
+                .getList("data", Users.class);
+    }
+
+    public Users getSingleUser(){
+        return given().spec(REQ_SPEC).basePath("users/2")
+                .when()
+                .get()
+                .then()
+                .extract()
+                .jsonPath()
+                .getObject("data", Users.class);
+    }
+    public UpdateUserResponce updateUser(){
+        UpdateUserRequest ur = UserGenerator.updateUser();
+        return  given().spec(REQ_SPEC)
+                .basePath("users/2")
+                .body(ur)
+                .when().put().then().extract().as(UpdateUserResponce.class);
+    }
+
+    public UpdateUserResponce patchUser(){
+        UpdateUserRequest ur = UserGenerator.updateUser();
+        return  given().spec(REQ_SPEC)
+                .basePath("users/2")
+                .body(ur)
+                .when().patch().then().extract().as(UpdateUserResponce.class);
+    }
+    public void deleteUser(){
+        given().spec(REQ_SPEC).basePath("users/2").when().delete().then().statusCode(204);
+    }
+}
